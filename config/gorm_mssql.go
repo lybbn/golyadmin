8c2831fb3d0b0f8dc1363df963147e006e2ac5fc
lybbn <yanwei@qq.com>
2023-06-05 18:30:26 +0800

    数据库配置修改

--- config/gorm_mssql.go
+++ config/gorm_mssql.go
@@ -4,7 +4,7 @@ type Mssql struct {
 	GeneralDB `yaml:",inline" mapstructure:",squash"`
 }
 
-// dsn := "sqlserver://gorm:LoremIpsum86@localhost:9930?database=gorm"
+// dsn := "sqlserver://gorm:root@localhost:1433?database=gorm"
 func (m *Mssql) Dsn() string {
 	return "sqlserver://" + m.Username + ":" + m.Password + "@" + m.Path + ":" + m.Port + "?database=" + m.Dbname + "&encrypt=disable"
 }
