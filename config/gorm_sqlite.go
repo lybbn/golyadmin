aa61d86be01c3099a066e85e8b48b26c62711eb1
lybbn <yanwei@qq.com>
2023-06-08 18:37:44 +0800

    问题修改

---
+++ config/gorm_sqlite.go
@@ -0,0 +1,14 @@
+package config
+
+type Sqlite struct {
+	GeneralDB `yaml:",inline" mapstructure:",squash"`
+}
+
+// dsn := "sqlserver://gorm:root@localhost:1433?database=gorm"
+func (m *Mssql) Dsn() string {
+	return "sqlserver://" + m.Username + ":" + m.Password + "@" + m.Path + ":" + m.Port + "?database=" + m.Dbname + "&encrypt=disable"
+}
+
+func (m *Mssql) GetLogMode() string {
+	return m.LogMode
+}
