78bfb9aacc29ebb14d410482154fd871fcfa85f9
lybbn <1042594286@qq.com>
2023-07-04 23:52:09 +0800

    框架优化

---
+++ backend/utils/api/lyapi.go
@@ -0,0 +1,32 @@
+// 数据库操作工具类
+package api
+
+import (
+	"gitee.com/lybbn/golyadmin/global"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type LyApi struct {
+	Context *gin.Context
+	Orm     *gorm.DB
+	Errors  error
+}
+
+// SetContext 设置gin http上下文Context
+func (a *LyApi) SetContext(c *gin.Context) *LyApi {
+	a.Context = c
+	return a
+}
+
+// SetOrm 自定义设置*gorm.DB数据库连接
+func (a *LyApi) SetOrm(db *gorm.DB) *LyApi {
+	a.Orm = db
+	return a
+}
+
+// DefaultOrm 设置默认*gorm.DB数据库连接
+func (a *LyApi) SetDefaultOrm() *LyApi {
+	a.Orm = global.GL_DB
+	return a
+}
