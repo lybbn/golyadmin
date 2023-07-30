34f9b0646c7076dd6518135cac01b1537d70a9fd
lybbn <1042594286@qq.com>
2023-07-30 22:11:48 +0800

    readme

--- backend/global/api_white.go
+++ backend/global/api_white.go
@@ -6,7 +6,7 @@ type urlInfo struct {
 	DataSource bool //数据权限是否也是白名单 true 是白名单、false 否
 }
 
-// 权限验证需要排除的路由列表 GET\POST\PUT\DELETE
+// 权限验证需要排除的路由列表 ，Method GET\POST\PUT\DELETE
 var GL_API_WHILTELIST = []urlInfo{
 	{Api: "/api/system/menu/web_router", Method: "GET", DataSource: false},
 	{Api: "/api/system/file/uploadFile", Method: "POST", DataSource: true},
